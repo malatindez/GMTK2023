70cd035898838cfec0417a4b75e74e84d3511d8c
nullsoftware <tumyrthebot@gmail.com>
2023-07-09 14:24:30 +0300

    Added pause menu.

---
+++ Assets/Scripts/ButtonFocusManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonFocusManager : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField] private Text _text;
+    [SerializeField] private bool _isDefault;
+
+    private RectTransform _rectTransform;
+    private Button _button;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        FocusVisual.Instance.SetTarget(_button);
+    }
+
+    private void Start()
+    {
+        _button = GetComponent<Button>();
+        _rectTransform = GetComponent<RectTransform>();
+
+        if (_text == null)
+            _text = GetComponentInChildren<Text>();
+    }
+
+    private void OnEnable()
+    {
+        if (_isDefault && FocusVisual.Instance != null)
+        {
+            FocusVisual.Instance.SetTarget(_button);
+        }
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _menu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_menu.activeSelf)
+            {
+                _menu.SetActive(false);
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                _menu.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
